6e8245d20fec6eb77221add01050d9c5f8bf3bee
Eric Soppi <soppi.2@osu.edu>
2017-05-13 11:22:03 -0400

    Added gitignore and tokentypes

---
+++ MyCompiler/ProgramNodes/IBooleanExpressionChild.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace MyCompiler.ProgramNodes
+{
+    public interface IBooleanExpressionChild
+    {
+
+    }
+}
---
+++ MyCompiler/ProgramNodes/IExpressionChild.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace MyCompiler.ProgramNodes
+{
+    public interface IExpressionChild
+    {
+    }
+}
