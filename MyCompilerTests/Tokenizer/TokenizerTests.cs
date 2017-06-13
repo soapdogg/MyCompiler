0a1cdde543f5631d3fb2ce36487a4526bb2d86d7
Eric A. Soppi <soppi.2@osu.edu>
2017-06-13 15:31:13 -0400

    Added basic layout of the tokenizer tests

--- MyCompilerTests/Tokenizer/TokenizerTests.cs
+++ MyCompilerTests/Tokenizer/TokenizerTests.cs
@@ -9,8 +9,157 @@ namespace MyCompilerTests.Tokenizer
         private MyCompiler.Tokenizer.Tokenizer t;
 
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test1.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test2()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test2.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test3()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test3.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test4()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test4.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test5()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test5.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test6()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test6.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test7()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test7.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test8()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test8.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test9.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test10.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test11()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test11.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test12()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test12.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test13()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test13.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test14()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test14.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test15()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test15.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test16()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test16.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test17()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test17.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test18()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test18.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test19()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test19.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test20()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test20.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test21()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test21.c");
+            t.Tokenize();
+        }
+
+        [TestMethod]
+        public void Test22()
+        {
+            t = new MyCompiler.Tokenizer.Tokenizer(@"Data", "test22.c");
+            t.Tokenize();
         }
     }
 }
